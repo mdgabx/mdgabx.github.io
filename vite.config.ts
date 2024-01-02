ccdc3692fcc1bd65757ceaf4a85914c8c4f2c1e4
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-01-02 10:33:19 +0800

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import autoprefixer from 'autoprefixer';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mdgabx.github.io",
+  base: "./",
   css: {
     postcss: {
       plugins: [
