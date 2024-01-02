517086c92fc2f129aef7bc6b4fea39beb7ff3533
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-01-02 10:14:08 +0800

    add: styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./App.css"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from "./components/Layout"
+import Skills from "./components/Skills"
+import Home from "./components/Home"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
+
+const router = createBrowserRouter([
+  { path: "/", element: <Layout><Home /></Layout> },
+  { path: '/skills', element: <Layout><Skills /></Layout> },
+  { path: '/projects', element: <Layout><Projects /></Layout> },
+  { path: '/contact', element: <Layout><Contact /></Layout> }
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
